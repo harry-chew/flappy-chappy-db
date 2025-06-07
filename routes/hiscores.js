4dc99ec21b6b6840f0535d99c3878b9c3f845fb0
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2025-06-07 15:44:57 +0100

    post hiscore route with UUID from device and only updates if score is higher than currently stored

--- routes/hiscores.js
+++ routes/hiscores.js
@@ -14,15 +14,40 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { player, score } = req.body;
-  if (!player || !score) {
+  const { player, score, uuid } = req.body;
+  if (!player || !score || !uuid) {
+    console.error('Missing player or score or uuid in request body');
     return res.status(400).json({ error: 'Player and score are required' });
   }
   try {
+
+    // Check if the player already has a high score
+    const [existing] = await pool.promise().query(
+      'SELECT * FROM hiscores WHERE uuid = ?',
+      [uuid]
+    );
+
+    if (existing.length > 0) {
+      console.log(existing);
+      // If the player exists, update their score if the new score is higher
+      if (existing[0].score >= score) {
+        return res.status(400).json({ error: 'Score must be higher than the existing score' });
+      }
+      console.log(`Updating high score for ${player}: ${score}`);
+      const [update] = await pool.promise().query(
+        'UPDATE hiscores SET score = ?, player = ? WHERE uuid = ?',
+        [score, player, uuid]
+      );
+      if (update.affectedRows === 0) {
+        return res.status(400).json({ error: 'Failed to update score' });
+      }
+      return res.status(200).json({ message: 'Score updated successfully' });
+    }
+
     console.log(`Saving high score for ${player}: ${score}`);
     const [result] = await pool.promise().query(
-      'INSERT INTO hiscores (player, score) VALUES (?, ?)',
-      [player, score]
+      'INSERT INTO hiscores (player, score, uuid) VALUES (?, ?, ?)',
+      [player, score, uuid]
     );
     
     if (result.affectedRows === 0) {
